b0d74cc65ad3c7d37b022e0c1634eddc43321b2a
nnahcus <suchanyac23@gmail.com>
2026-01-31 20:46:33 +0700

    middleware update

--- middleware.ts
+++ middleware.ts
@@ -6,15 +6,20 @@ const PUBLIC_ROUTES =["/login", "/register", "/"]
 const API_ROUTES = "/api/auth"
 
 export default auth((req) => {
-  const isLoggedIn = !!req.auth
-  const { nextUrl } = req
+  try {
+    const isLoggedIn = !!req.auth
+    const { nextUrl } = req
 
-  const isPublicRoute = PUBLIC_ROUTES.includes(nextUrl.pathname)
-  const isApiRoute = nextUrl.pathname.startsWith(API_ROUTES)
-  if (isApiRoute) return NextResponse.next();
-  if (isLoggedIn && isPublicRoute) return NextResponse.redirect(new URL(DEFAULT_REDIRECT, nextUrl));
-  if (!isLoggedIn && !isPublicRoute) return NextResponse.redirect(new URL('/login', nextUrl));
-  return NextResponse.next()
+    const isPublicRoute = PUBLIC_ROUTES.includes(nextUrl.pathname)
+    const isApiRoute = nextUrl.pathname.startsWith(API_ROUTES)
+    if (isApiRoute) return NextResponse.next();
+    if (isLoggedIn && isPublicRoute) return NextResponse.redirect(new URL(DEFAULT_REDIRECT, nextUrl));
+    if (!isLoggedIn && !isPublicRoute) return NextResponse.redirect(new URL('/login', nextUrl));
+    return NextResponse.next()
+  } catch (error) {
+    console.error('Middleware error:', error)
+    return NextResponse.next()
+  }
 })
  
 // Optionally, don't invoke Middleware on some paths
